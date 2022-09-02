48fe9abeec39fa8cbb7110267fb4376068862cd7
Сергей <privet15.89@mail.ru>
2022-09-02 12:24:00 +0500

    hide error when input not empty

--- src/p2-homeworks/h3/Greeting.tsx
+++ src/p2-homeworks/h3/Greeting.tsx
@@ -1,23 +1,24 @@
-import React, {ChangeEvent} from 'react'
+import React, {ChangeEvent, KeyboardEvent} from 'react'
 import s from './Greeting.module.css'
 
 type GreetingPropsType = {
     name: string // need to fix any
     setNameCallback: (e: ChangeEvent<HTMLInputElement>) => void // need to fix any
     addUser: (name: string) => void // need to fix any
-    error: string // need to fix any
+    error: string | null // need to fix any
     totalUsers: number // need to fix any
+    pressEnter: (e: KeyboardEvent<HTMLInputElement>) => void
 }
 
 // презентационная компонента (для верстальщика)
 const Greeting: React.FC<GreetingPropsType> = (
-    {name, setNameCallback, addUser, error, totalUsers} // деструктуризация пропсов
+    {name, setNameCallback, addUser, error, totalUsers, pressEnter} // деструктуризация пропсов
 ) => {
     const inputClass = error ? s.error : "" // need to fix with (?:)
 
     return (
         <div>
-            <input value={name} onChange={setNameCallback} className={inputClass}/>
+            <input value={name} onKeyDown={pressEnter} onChange={setNameCallback} className={inputClass}/>
             <button onClick={() => addUser(name)}>add</button>
             <span>{totalUsers}</span>
             {error && <div className={s.errorMessage}>{error}</div>}
--- src/p2-homeworks/h3/GreetingContainer.tsx
+++ src/p2-homeworks/h3/GreetingContainer.tsx
@@ -1,4 +1,4 @@
-import React, {ChangeEvent, useState} from 'react'
+import React, {ChangeEvent, useState, KeyboardEvent} from 'react'
 import Greeting from './Greeting'
 import {UserType} from "./HW3";
 
@@ -14,7 +14,7 @@ type GreetingContainerPropsType = {
 // уровень локальной логики
 const GreetingContainer: React.FC<GreetingContainerPropsType> = ({users, addUserCallback}) => { // деструктуризация пропсов
     const [name, setName] = useState<string>('') // need to fix any
-    const [error, setError] = useState<string>('') // need to fix any
+    const [error, setError] = useState<string | null>('') // need to fix any
 
     const setNameCallback = (e: ChangeEvent<HTMLInputElement>) => { // need to fix any
         const currentName = e.currentTarget.value;
@@ -32,6 +32,13 @@ const GreetingContainer: React.FC<GreetingContainerPropsType> = ({users, addUser
         }
     }
 
+    const pressEnter = (e: KeyboardEvent<HTMLInputElement>) => {
+        setError(null)
+        if (e.key === "Enter") {
+         addUser(name)
+        }
+    }
+
     const totalUsers = users.length // need to fix
 
     return (
@@ -41,7 +48,7 @@ const GreetingContainer: React.FC<GreetingContainerPropsType> = ({users, addUser
             addUser={addUser}
             error={error}
             totalUsers={totalUsers}
-        />
+            pressEnter={pressEnter}/>
     )
 }
 
