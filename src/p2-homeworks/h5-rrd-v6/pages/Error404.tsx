33c6a792ad6f9274b0bfa0bbdb07f91f77df30cd
Сергей <privet15.89@mail.ru>
2022-09-17 11:25:39 +0500

    hw5

---
+++ src/p2-homeworks/h5-rrd-v6/pages/Error404.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Error404() {
+    return (
+        <div>
+            {/*<div>404</div>
+            <div>Page not found!</div>
+            <div>—ฅ/ᐠ.̫ .ᐟ\ฅ—</div>*/}
+            <img src={'https://img.freepik.com/free-vector/oops-404-error-with-a-broken-robot-concept-illustration_114360-5529.jpg?w=500'}/>
+        </div>
+    )
+}
+
+export default Error404
