335c3ca9f14d8b627e9d5e28f03a530b3e03c1a7
Сергей <privet15.89@mail.ru>
2022-10-11 16:25:43 +0500

    hw08

---
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -0,0 +1,26 @@
+import {UserType} from '../HW8'
+
+type ActionType =
+    | { type: 'sort'; payload: 'up' | 'down' }
+    | { type: 'check'; payload: number }
+
+export const homeWorkReducer = (state: Array<UserType>, action: ActionType): Array<UserType> => { // need to fix any
+    switch (action.type) {
+        case 'sort': { // by name
+            let stateCopy = state
+            if (action.payload === 'up') {
+                stateCopy.sort((a, b) => a.name.localeCompare(b.name))
+            }
+            if (action.payload === 'down') {
+                stateCopy.sort((a, b) => b.name.localeCompare(a.name))
+            }
+            return stateCopy// need to fix
+        }
+        case 'check': {
+
+            return state.filter(el => el.age > 18) // need to fix
+        }
+        default:
+            return state
+    }
+}
