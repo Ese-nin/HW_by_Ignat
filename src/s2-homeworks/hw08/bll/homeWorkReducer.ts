0076fe19d101a89cfdb5961e5f5faf2503f689e7
Сергей <privet15.89@mail.ru>
2022-10-11 16:31:21 +0500

    hw08 fixed

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -17,8 +17,8 @@ export const homeWorkReducer = (state: Array<UserType>, action: ActionType): Arr
             return stateCopy// need to fix
         }
         case 'check': {
-
-            return state.filter(el => el.age > 18) // need to fix
+            let stateCopy = state
+            return stateCopy.filter(el => el.age > 18).sort((a,b)=>a.age-b.age) // need to fix
         }
         default:
             return state
