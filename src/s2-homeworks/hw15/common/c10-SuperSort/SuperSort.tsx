0e85b69c1bab0cc99baf44956c4a76a143a7f7b3
Сергей <privet15.89@mail.ru>
2022-12-19 23:29:21 +0500

    hw15 (without css)

---
+++ src/s2-homeworks/hw15/common/c10-SuperSort/SuperSort.tsx
@@ -0,0 +1,70 @@
+import React from 'react'
+import downArrow from '../icons/down-arrow.png'
+import upArrow from '../icons/up-arrow.png'
+import none from '../icons/none.jpg'
+
+// добавить в проект иконки и импортировать
+const downIcon = downArrow
+const upIcon = upArrow
+const noneIcon = none
+
+
+export type SuperSortPropsType = {
+    id?: string
+    sort: string
+    value: string
+    onChange: (newSort: string) => void
+}
+
+export const pureChange = (sort: string, down: string, up: string) => {
+    // пишет студент, sort: (click) => down (click) => up (click) => '' (click) => down ...
+    let temp = '';
+    if (sort === '') {
+        temp = down
+    } else if (sort === down) {
+        temp = up
+    } else if (sort === up) {
+        temp = ''
+    } else if (sort !== down && sort !== up) {
+        temp = down
+    }
+    return temp
+    // return up // исправить
+}
+
+const SuperSort: React.FC<SuperSortPropsType> = (
+    {
+        sort, value, onChange, id = 'hw15',
+    }
+) => {
+    const up = '0' + value
+    const down = '1' + value
+
+    const onChangeCallback = () => {
+        onChange(pureChange(sort, down, up))
+    }
+
+    const icon = sort === down
+        ? downIcon
+        : sort === up
+            ? upIcon
+            : noneIcon
+
+    return (
+        <span
+            id={id + '-sort-' + value}
+            onClick={onChangeCallback}
+        >
+            {/*сделать иконку*/}
+            <img
+                id={id + '-icon-' + sort}
+                src={icon}
+                alt={''}
+            />
+
+            {/*{icon} а это убрать*/}
+        </span>
+    )
+}
+
+export default SuperSort
